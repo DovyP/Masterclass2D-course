887db5a39f6f26ead40fde896edbd481454f2f84
DovyP <translatefunmira@gmail.com>
2023-02-07 10:03:42 +0200

    Added collision layers to determine which colliders interact with each other

--- Assets/Scripts/PlayerProjectileController.cs
+++ Assets/Scripts/PlayerProjectileController.cs
@@ -20,9 +20,6 @@ public class PlayerProjectileController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag != "Player")
-        {
-            Destroy(gameObject);
-        }
+        Destroy(gameObject);
     }
 }

278f1abe973b9e0c806fc3451190009e4dc1446c
DovyP <translatefunmira@gmail.com>
2023-02-07 05:47:53 +0200

    Added pistol projectile sprites and logic in scripts

---
+++ Assets/Scripts/PlayerProjectileController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectileController : MonoBehaviour
+{
+    [SerializeField] float projectileSpeed = 5f;
+
+    private Rigidbody2D projectileRigidbody;
+
+    void Start()
+    {
+        projectileRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        projectileRigidbody.velocity = transform.right * projectileSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+}

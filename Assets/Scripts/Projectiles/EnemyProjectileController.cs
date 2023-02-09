dc6c7a9d6cf22dbaea48ab9b2931b200a519c931
DovyP <translatefunmira@gmail.com>
2023-02-09 13:18:02 +0200

    Created a projectile for the slime enemy

---
+++ Assets/Scripts/Projectiles/EnemyProjectileController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectileController : MonoBehaviour
+{
+    [SerializeField] float projectileSpeed;
+    private Vector3 playerDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerDirection = FindObjectOfType<PlayerController>().transform.position - transform.position;
+        playerDirection.Normalize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += playerDirection * projectileSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Player hit!");
+        }
+        
+        Destroy(gameObject);
+    }
+}

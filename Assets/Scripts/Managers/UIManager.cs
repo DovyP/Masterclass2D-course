357da0fcd47b7a53394034d69eab79380e10c540
DovyP <translatefunmira@gmail.com>
2023-02-14 18:52:46 +0200

    Created FadeIn and FadeOut animations using UI Canvas when scenes change

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    [SerializeField] Image imageToFade;
+
+    private void Start()
+    {
+        instance = this;
+    }
+
+    public void FadeImage()
+    {
+        imageToFade.GetComponent<Animator>().SetTrigger("Start Fade");
+    }
+}
